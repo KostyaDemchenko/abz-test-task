45d22e66ae3c59ccba28a47b3e19b32c251cd80c
KostyaDemchenko <kostya.doge@gmail.com>
2024-05-23 00:25:53 +0300

    Created button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+const Button = ({ children }: ButtonProps) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+
+// importing components
+import Button from "@/components/Button";
+
 import "./global.scss";
 
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main>
+      <Button>Button</Button>
+    </main>
+  );
 }
