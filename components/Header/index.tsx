db3f33adf0c1677ebe9568f6f0b2a5f7e29b77eb
KostyaDemchenko <kostya.doge@gmail.com>
2024-05-23 16:19:30 +0300

    Fixed deploy; Updated button component; Added First block; Updated css shortClasses (there was a problem with recursion)

---
+++ components/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+
+import Button from "@/components/Button";
+
+import logo from "@/public/logo.svg";
+
+import "./style.scss";
+
+const Headder = () => {
+  return (
+    <header>
+      <div className='container'>
+        <Image src={logo} alt='logo' />
+        <div className='btn-box'>
+          <Button>Users</Button>
+          <Button>Sign up</Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Headder;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 // importing components
+import Headder from "@/components/Header";
+import FirstSection from "@/components/FirstSection";
 import Button from "@/components/Button";
 
 import "./global.scss";
 
 export default function Home() {
   return (
-    <main>
-      <Button>Button</Button>
-    </main>
+    <>
+      <Headder />
+      <main>
+        <FirstSection />
+      </main>
+    </>
   );
 }
